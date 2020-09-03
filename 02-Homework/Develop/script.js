4e80461a3bac7e7e3d4a47aca9a50a7526822efc
Tzcodes101 <68740427+Tzcodes101@users.noreply.github.com>
2020-09-03 08:47:27 -0400

    html update

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -11,20 +11,30 @@ $(document).ready(function () {
         //get the value of the textarea sibling
         var text = $(this).siblings(".description").val();
         //keep track of the scheduleHour by noting the id of the parent (which was set in html)
+        var scheduleHour = $(this).parent().attr("id");
 
         // Trim text, trim schedule hour
+        text.trim();
+        scheduleHour.trim();
 
         //save both of these values to local storage
-
+        localStorage.setItem(text, scheduleHour);
         //call readFromLocalStorage
     })
-
+    timeTracker();
     //we must keep track of the time as it progresses with a timeTracker function
+    function timeTracker () {
         //that obtains the current hour
+        var currentHour = moment().hours();
+        console.log(currentHour);
 
-        //in a for loop for each timeblock
+        //in a for each loop for each timeblock
+        $(".time-block").each(function () {
             //checks to see if we've moved past this time
+            
             //changing class to past present or future accordingly, based on comparison to current hour (,, === or =,=, ===)
+        })       
+    }
 
     //Set time to update every 20 seconds
 
